3db8a799163490c01267aa2ad74d19c19bc172a3
CesarJustiniano <cesar.justiniano@upr.edu>
2019-04-22 14:00:41 -0400

    updated shell.py

--- src/shell.py
+++ src/shell.py
@@ -1,8 +1,21 @@
 from src import Datavity
 
 while True:
-	text = input('Datavity > ')
-	result, error = Datavity.run('<stdin>', text)
+    text = input('Datavity > ')
 
-	if error: print(error.as_string())
-	elif result: print(result)
+    if text == "exit":
+        print("Thank you for using Datavity!")
+        break
+    elif text == "help":
+        print("Datavity: The Feature Engineering Language\n")
+        print("\tShow the title of all colums: FEATURE()\n")
+        print("\tShows 4 histograms, without change, applying linear scaling, log scaling and clipping: "
+              "TRANSFORM(dataFrame)\n")
+        print("\tShow first 20 rows of dataFrame: DESCRIBE()\n")
+        print("\tExit Datavity: exit\n")
+    else:
+        result, error = Datavity.run('<stdin>', text)
+        if error:
+            print(error.as_string())
+        elif result:
+            print(result)
