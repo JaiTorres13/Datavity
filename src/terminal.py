cd1c24c2f7c253808ebe4a44a94cb5810af6eb62
CesarJustiniano <cesar.justiniano@upr.edu>
2019-04-23 18:29:52 -0400

    commented in terminal.py

--- src/terminal.py
+++ src/terminal.py
@@ -3,6 +3,7 @@ import ply.yacc as yacc
 from src import Datavity as fe
 
 
+# Command guide to help user understand this code
 def help():
     print('''Datavity: The Feature Engineering language.\n
             Display the title of each columns: features() \n 
@@ -13,7 +14,7 @@ def help():
     ''')
 
 
-# lex part
+# lexer part
 reserved = {
     '(': 'LPAREN',
     ')': 'RPAREN',
@@ -49,23 +50,27 @@ def t_error(t):
 lexer = lex.lex()
 
 
-# yacc part
-# Parser
+# Parser part
+# The following methods follow from top to bottom
 
+# This is the first method to work for each input from user
 def p_define(p):
     '''define : features
             | clean
             | transform
             | rmse'''
 
+    # p works as a list and each index is a token from lexer
     p[0] = p[1]
 
 
+# reachable when the user enters: features()
 def p_features(p):
     'features : FEATURES LPAREN RPAREN'
     fe.FEATURES()
 
 
+# reachable when the user enters for example: clean(ZERO)
 def p_clean(p):
     'clean : CLEAN LPAREN ID RPAREN'
     # treat p as if it were a list; each separate word is a cell in the list.
@@ -75,12 +80,15 @@ def p_clean(p):
         print("Error: parameter expected to be MAX, MIN, MEAN or ZERO. Instead of '" + p[3] + "'")
 
 
+# reachable when the user enters for example: transform(price)
 def p_transform(p):
     'transform : TRANSFORM LPAREN ID RPAREN'
+    # It makes sure that ID is from the features shown in the fe.FEATURES() method
     if not fe.TRANSFORM(p[3]):
         print("Error: '" + p[3] + "' is not part of the numeric feature")
 
 
+# reachable when the user enters for example: rmse(ZERO, price, losses)
 def p_rmse(p):
     'rmse : RMSE LPAREN ID COMMA ID COMMA ID RPAREN'
     if (p[3] == 'MAX' or p[3] == 'MIN' or p[3] == 'MEAN' or p[3] == 'ZERO') and p[5] != p[7]:
@@ -90,11 +98,14 @@ def p_rmse(p):
         print("Error: invalid parameter(s)")
 
 
+# if none of the previous methods from parser were reached, then this one activates by default
 def p_error(p):
     print('Syntax error in code provided!', s)
 
 
 parser = yacc.yacc()
+
+# Runs until user enters 'exit' to end the code
 while True:
     try:
         s = input('Datavity > ')
